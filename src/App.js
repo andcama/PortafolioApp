8c17bb4ee87712298254f055a81c20d0e88bc6c0
Andres Camacho Aguilar <andcamachoa@gmail.com>
2020-05-07 16:06:57 -0600

    final changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
 import $ from "jquery";
-import "./App.css";
 import Header from "./Componentes/Header";
 import Footer from "./Componentes/Footer";
 import AcercaDe from "./Componentes/AcercaDe";
